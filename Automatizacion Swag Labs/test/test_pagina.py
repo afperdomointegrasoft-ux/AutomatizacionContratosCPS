74e3516601af46eb95736ef851b2736cb2a04eab
afperdomo <108319181+afperdomo@users.noreply.github.com>
2023-11-11 22:05:18 -0500

    Nuevos CP y ajustes

--- Automatizacion Swag Labs/test/test_pagina.py
+++ Automatizacion Swag Labs/test/test_pagina.py
@@ -11,7 +11,19 @@ class pagina(unittest.TestCase):
     def test_01_login(self):
         self.home.ingreso_pagina("standard_user","secret_sauce")
 
+    def test_02_val_campo_req(self):
+        self.home.ingreso_pagina(" "," ")
+        self.home.val_camp_obli(2)
 
+    def test_03_seleccionarProducto(self):
+        self.home.ingreso_pagina("standard_user", "secret_sauce")
+        self.home.seleccionar_producto()
+
+    def test_04_checkout(self):
+        self.home.ingreso_pagina("standard_user", "secret_sauce")
+        self.home.seleccionar_producto()
+        self.home.checkout("Andres","Perdomo","420005")
+        self.home.finalizar_compra()
 
     def tearDown(self):
         time.sleep(1)
